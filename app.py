3ff0e4dcefa10e27c1adda90db1393b2f8dc66ab
caritoespicaita <caritoespicaita@hotmail.com>
2022-06-16 19:15:17 -0500

    update

--- app.py
+++ app.py
@@ -1,15 +1,100 @@
-#!/usr/bin/python3
-""" Starts Flask. """
+from crypt import methods
+from flask import render_template, request, redirect
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from route.routes import routes
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 
-#conexion a la base de datos toca crear un usuario
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:adminpwd@localhost/mi_negocio'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'adminpwd'
+app.config['MYSQL_DB'] = 'mi_negocio'
 
-SQLAlchemy(app)
 
-app.register_blueprint(routes)
+mysql = MySQL(app)
+
+#metodo get
+@app.route('/')
+def index_route():
+    """ inicio """
+    return render_template('index.html')
+
+@app.route('/new_client', methods=['POST'])
+def addclient_route():
+    """ new client """
+    if mysql:
+        print("Connection Successful!")
+        cursor = mysql.connection.cursor()
+    else:
+        print("Connection Failed!")
+
+    if request.method == 'POST':
+        
+        cedula = request.form['cedula']
+        name = request.form['name']
+        cursor.execute('INSERT INTO cliente (id_cliente, name) VALUES(%s, %s)',
+        (cedula, name))
+        mysql.connection.commit()
+        
+        return redirect('/')
+    
+@app.route('/new_supplier', methods=['POST'])
+def addsupp_route():
+    """ new supp """
+    if mysql:
+        print("Connection Successful!")
+        cursor = mysql.connection.cursor()
+    else:
+        print("Connection Failed!")
+
+    if request.method == 'POST':
+        
+        nit = request.form['nit']
+        name = request.form['name']
+        location = request.form['location']
+        cursor.execute('INSERT INTO proveedor (id_proveedor, name, ubicacion) VALUES(%s, %s, %s)',
+        (nit, name, location))
+        mysql.connection.commit()
+        
+        return redirect('/')
+
+
+@app.route('/new_product', methods=['POST'])
+def addproduct_route():
+    """ new product """
+    if mysql:
+        print("Connection Successful!")
+        cursor = mysql.connection.cursor()
+    else:
+        print("Connection Failed!")
+
+    if request.method == 'POST':
+        
+        cod_prod = request.form['codigo_producto']
+        ser_prod = request.form['serial_producto']
+        tipo = request.form['tipo']
+        precio = request.form['precio']
+        cursor.execute('INSERT INTO producto(codigo_producto, serial_producto, tipo, precio) VALUES(%s, %s, %s, %s)',
+        (cod_prod, ser_prod, tipo, precio))
+        mysql.connection.commit()
+        
+        return redirect('/')
+    
+
+@app.route('/edit')
+def edit_route():
+    """ new product """
+    
+    return "editado"
+
+@app.route('/delete')
+def delete_route():
+    """ new product """
+    
+    return"borrado"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
--- index.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from models.engine.db import db
-
-with app.app_context():
-    db.create_all()
-    
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
